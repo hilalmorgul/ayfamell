3c39c82f78407f6be3acb0c4c99e216dd4f5b350
Hilal MORGÜL <hilalmrgl@icloud.com>
2024-12-14 12:34:21 +0300

    css

---
+++ iletisim.php
@@ -0,0 +1,63 @@
+<?php 
+    include('header.php')
+?>
+
+
+
+        <!--Page Header Start-->
+        <section class="page-header">
+            <div class="page-header-bg" style="background-image: url(assets/images/backgrounds/services.webp)">
+            </div>
+            <div class="page-header__overly"></div>
+            <div class="container">
+                <div class="page-header__inner">
+             
+                    <h2>İLETİŞİM</h2>
+                </div>
+            </div>
+        </section>
+        <!--Page Header End-->
+
+
+
+        <!--Location Start-->
+        <section class="location">
+            <div class="location-shape-1 float-bob-x">
+                <img src="assets/images/shapes/location-shape-1.png" alt="">
+            </div>
+            <div class="container">
+                <div class="row">
+                    <!--Location single Start-->
+                    <div class="col-xl-8 col-lg-8 col-md-8">
+                        <div class="location__single">
+                            <h4 class="location__title">AYFAMEL ADRES</h4>
+                            <p class="location__text">22 Texas West Hills 22 Texas West Hills 22 Texas West Hills</p>
+                            <div class="location__phone-email">
+                                <a href="mailto:info@ayfamel.com">info@ayfamel.com</a> <br>
+                                <a href="tel:9200888690">+90 (530) - 609 1996</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Location single End-->
+                 
+                </div>
+            </div>
+        </section>
+        <!--Location End-->
+
+        <!--Google Map Start-->
+        <section class="contact-page-google-map">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
+                class="google-map__two" allowfullscreen></iframe>
+
+        </section>
+
+  
+
+
+
+    </div><!-- /.page-wrapper -->
+<?php 
+    include('footer.php')
+?>
